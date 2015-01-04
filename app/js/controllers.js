3aa8eda0d0168e25155b6537e6f86e1395c2d379
Federico Vezzoli <federico@dot-agency.it>
2015-01-04 12:05:55 +0100

    continued the pagination

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,13 +7,34 @@ var rememberthatControllers = angular.module('rememberthatControllers', []);
 //do a basic query, get 10 latest libraries entry
 rememberthatControllers.controller('RememberthatCtrl', ['$scope', 'Entry',
   function($scope, Entry) {
+
 	//$scope.itemsPerPage = 5; /* set in wp no need to overwrite it right now */
-    $scope.currentPage = 1;
-    $scope.entries = Entry.query({page: $scope.currentPage}, function(l,getResponseHeaders){
-	    //console.log(getResponseHeaders("X-WP-TotalPages"));
-		$scope.totalPages = getResponseHeaders("X-WP-TotalPages");
-		$scope.totalArticles = getResponseHeaders("X-WP-Total");
-    });
+	$scope.currentPage = 1;
+
+	//print results
+	$scope.printResults = function(){
+		$scope.entries = Entry.query({page: $scope.currentPage}, function(l,getResponseHeaders){
+	    	//console.log(getResponseHeaders("X-WP-TotalPages"));
+			$scope.totalPages = getResponseHeaders("X-WP-TotalPages");
+			$scope.totalArticles = getResponseHeaders("X-WP-Total");
+		});
+	};
+
+
+	//pagination
+
+
+	//update pagination
+	$scope.updatePagination = function(newPage){
+		$scope.currentPage = newPage;
+		$scope.printResults();
+	};
+
+	//init
+	if ($scope.currentPage === 1) {
+		$scope.printResults();
+	}
+
  }]);
 
  //retrieve the categories
